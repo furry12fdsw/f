d01d2f477b5940b153e1ef3c796902ad47e9d4f9
furry12fdsw <145102040+furry12fdsw@users.noreply.github.com>
2023-09-21 22:47:58 +0900

    Update furry.py

--- furry.py
+++ furry.py
@@ -1,162 +1,77 @@
-from __future__ import annotations
-
 import os
+import sys
+import threading
 import time
+from pathlib import Path
+from typing import Callable, Optional
 
-from modules import timer
-from modules import initialize_util
-from modules import initialize
-
-startup_timer = timer.startup_timer
-startup_timer.record("launcher")
-
-initialize.imports()
-
-initialize.check_versions()
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.routing import APIRoute
+from gradio import Blocks
+from modules import paths, shared
+from modules.script_callbacks import on_app_started
 
 
-def create_api(app):
-    from modules.api.api import Api
-    from modules.call_queue import queue_lock
+class Patches:
+    def __init__(self, demo: Optional[Blocks], app: FastAPI) -> None:
+        self.demo = demo
+        self.app = app
 
-    api = Api(app, queue_lock)
-    return api
+        self.patch_gradio_route()
+        self.patch_data_dir_path()
 
+    def patch_gradio_route(self):
+        """
+        Gradio 에서 `/file={path} 경로가 앱 경로와 다른 장치에 위치할 때
+        `Path.resolve().parents` 값 사용으로 인해 하위 디렉터리가 아닌 것으로 인식해
+        접근할 수 없는 이슈를 해결하고자 기존 엔드포인트 함수를 재정의합니다. 
+        """
+        original_endpoint: Optional[Callable] = None
 
-def api_only():
-    from fastapi import FastAPI
-    from modules.shared_cmd_options import cmd_opts
+        async def endpoint(path: str, *args, **kwargs):
+            original_error: ValueError
 
-    initialize.initialize()
+            try:
+                assert original_endpoint
+                return await original_endpoint(path, *args, **kwargs)
+            except ValueError as e:
+                # `path` 가 `app.cwd` 속에 있는 경로가 아닌 경우에 ValueError 를 반환함
+                # https://github.com/gradio-app/gradio/blob/58b1a074ba342fe01445290d680a70c9304a9de1/gradio/routes.py#L272
+                original_error = e
 
-    app = FastAPI()
-    initialize_util.setup_middleware(app)
-    api = create_api(app)
+            # `Path.resolve()` 사용으로 인해 `app.cwd` 내에 있는 심볼릭 링크의 경우 ValueError 를 반환할 수 있음
+            # https://github.com/gradio-app/gradio/blob/58b1a074ba342fe01445290d680a70c9304a9de1/gradio/routes.py#L263-L270
+            parents = Path(path).absolute().parents
 
-    from modules import script_callbacks
-    script_callbacks.before_ui_callback()
-    script_callbacks.app_started_callback(None, app)
+            if Path(shared.data_path).absolute() not in parents:
+                raise original_error
 
-    print(f"Startup time: {startup_timer.summary()}.")
-    api.launch(
-        server_name="0.0.0.0" if cmd_opts.listen else "127.0.0.1",
-        port=cmd_opts.port if cmd_opts.port else 7861,
-        root_path=f"/{cmd_opts.subpath}" if cmd_opts.subpath else ""
-    )
+            return FileResponse(path, headers={'Accept-Ranges': 'bytes'})
 
+        for route in self.app.router.routes:
+            if not isinstance(route, APIRoute):
+                continue
 
-def webui():
-    from modules.shared_cmd_options import cmd_opts
+            # Gradio 에서 내부 파일에 접근하는 경로의 패턴
+            # https://github.com/gradio-app/gradio/blob/58b1a074ba342fe01445290d680a70c9304a9de1/gradio/routes.py#L248
+            if route.path != '/file={path:path}':
+                continue
 
-    launch_api = cmd_opts.api
-    initialize.initialize()
+            if route.dependant.call != original_endpoint:
+                original_endpoint = route.dependant.call  # type: ignore
+                route.dependant.call = endpoint
 
-    from modules import shared, ui_tempdir, script_callbacks, ui, progress, ui_extra_networks
-
-    while 1:
-        if shared.opts.clean_temp_dir_at_start:
-            ui_tempdir.cleanup_tmpdr()
-            startup_timer.record("cleanup temp dir")
-
-        script_callbacks.before_ui_callback()
-        startup_timer.record("scripts before_ui_callback")
-
-        shared.demo = ui.create_ui()
-        startup_timer.record("create ui")
-
-        if not cmd_opts.no_gradio_queue:
-            shared.demo.queue(64)
-
-        gradio_auth_creds = list(initialize_util.get_gradio_auth_creds()) or None
-
-        auto_launch_browser = False
-        if os.getenv('SD_WEBUI_RESTARTING') != '1':
-            if shared.opts.auto_launch_browser == "Remote" or cmd_opts.autolaunch:
-                auto_launch_browser = True
-            elif shared.opts.auto_launch_browser == "Local":
-                auto_launch_browser = not any([cmd_opts.listen, cmd_opts.share, cmd_opts.ngrok, cmd_opts.server_name])
-
-        app, local_url, share_url = shared.demo.launch(
-            share=cmd_opts.share,
-            server_name=initialize_util.gradio_server_name(),
-            server_port=cmd_opts.port,
-            ssl_keyfile=cmd_opts.tls_keyfile,
-            ssl_certfile=cmd_opts.tls_certfile,
-            ssl_verify=cmd_opts.disable_tls_verify,
-            debug=cmd_opts.gradio_debug,
-            auth=gradio_auth_creds,
-            inbrowser=auto_launch_browser,
-            prevent_thread_lock=True,
-            allowed_paths=cmd_opts.gradio_allowed_path,
-            app_kwargs={
-                "docs_url": "/docs",
-                "redoc_url": "/redoc",
-            },
-            root_path=f"/{cmd_opts.subpath}" if cmd_opts.subpath else "",
-        )
-
-        startup_timer.record("gradio launch")
-
-        # gradio uses a very open CORS policy via app.user_middleware, which makes it possible for
-        # an attacker to trick the user into opening a malicious HTML page, which makes a request to the
-        # running web ui and do whatever the attacker wants, including installing an extension and
-        # running its code. We disable this here. Suggested by RyotaK.
-        app.user_middleware = [x for x in app.user_middleware if x.cls.__name__ != 'CORSMiddleware']
-
-        initialize_util.setup_middleware(app)
-
-        progress.setup_progress_api(app)
-        ui.setup_ui_api(app)
-
-        if launch_api:
-            create_api(app)
-
-        ui_extra_networks.add_pages_to_demo(app)
-
-        startup_timer.record("add APIs")
-
-        with startup_timer.subcategory("app_started_callback"):
-            script_callbacks.app_started_callback(shared.demo, app)
-
-        timer.startup_record = startup_timer.dump()
-        print(f"Startup time: {startup_timer.summary()}.")
-
-        try:
-            while True:
-                server_command = shared.state.wait_for_server_command(timeout=5)
-                if server_command:
-                    if server_command in ("stop", "restart"):
-                        break
-                    else:
-                        print(f"Unknown server command: {server_command}")
-        except KeyboardInterrupt:
-            print('Caught KeyboardInterrupt, stopping...')
-            server_command = "stop"
-
-        if server_command == "stop":
-            print("Stopping server...")
-            # If we catch a keyboard interrupt, we want to stop the server and exit.
-            shared.demo.close()
             break
 
-        # disable auto launch webui in browser for subsequent UI Reload
-        os.environ.setdefault('SD_WEBUI_RESTARTING', '1')
-
-        print('Restarting UI...')
-        shared.demo.close()
-        time.sleep(0.5)
-        startup_timer.reset()
-        script_callbacks.app_reload_callback()
-        startup_timer.record("app reload callback")
-        script_callbacks.script_unloaded_callback()
-        startup_timer.record("scripts unloaded callback")
-        initialize.initialize_rest(reload_script_modules=True)
+    def patch_data_dir_path(self):
+        """
+        `--data-dir` 인자를 사용하면 `extensions/` 등의 위치를 Import 하는 확장 기능들이 망가져버리므로
+        시스템 PATH 에 `--data-dir` 경로를 추가합니다.
+        """
+        sys.path.insert(0, paths.data_path)
 
 
-if __name__ == "__main__":
-    from modules.shared_cmd_options import cmd_opts
-
-    if cmd_opts.nowebui:
-        api_only()
-    else:
-        webui()
+on_app_started(
+    lambda demo, app: Patches(demo, app)
+)
